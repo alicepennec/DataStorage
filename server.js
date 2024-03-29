f3c2039a6f8405b424f469a2dd7212bb8994ef32
Alice <alicepennec@gmail.com>
2024-03-29 10:44:30 +0100

    Dockerfile created

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const sqlite3 = require('sqlite3');
+const dbname = 'main.db'
+
+let db = new sqlite3.Database(dbname, err => {
+  if(err)
+    throw err
+  console.log('Database stated on ' + dbname)
+
+})
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello server!</h1>');
+    res.send('<h1>Hello Simplon!</h1>');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
